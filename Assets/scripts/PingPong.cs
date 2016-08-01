969d4271694c259ae8a0e9765b6a7fe3e512901a
mohammadm8r <mrezaei76@yahoo.com>
2016-08-01 17:57:29 +0430

    second edition

---
+++ Assets/scripts/PingPong.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingPong : MonoBehaviour {
+
+    private Rigidbody2D myRigidbody;
+    float health;
+    public GameObject chocolate;
+	// Use this for initialization
+	void Start () {
+        myRigidbody = this.GetComponent<Rigidbody2D>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        switch(coll.gameObject.name)
+        {
+            case "chocolate":
+            Destroy (chocolate);
+			health++;
+			break;
+
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        myRigidbody.transform.position = new Vector2(Mathf.PingPong(Time.time * 3, 3.5f), myRigidbody.transform.position.y);
+	}
+}
--- Assets/scripts/PlayerHandler.cs
+++ Assets/scripts/PlayerHandler.cs
@@ -7,6 +7,8 @@ public class PlayerHandler : MonoBehaviour {
     [SerializeField]
     private float speed;
     private Vector2 vectorVelocity;
+    public float health;
+    public GameObject chocolate;
 
 	// Use this for initialization
 	void Start () {
@@ -15,13 +17,40 @@ public class PlayerHandler : MonoBehaviour {
         vectorVelocity.x = 3;
         vectorVelocity.y = 6;
         myRigidbody.velocity = vectorVelocity;
+        
 	}
-	
+
+ 
 	// Update is called once per frame
-    void update()
+    void Update()
     {
-        Debug.LogError(vectorVelocity);
+        velocityReducer();
     }
-   
-    
+
+    private void velocityReducer()
+    {
+        vectorVelocity = myRigidbody.velocity;
+       
+        if (vectorVelocity.y > 0)
+            vectorVelocity.y -= vectorVelocity.y / 150;
+        
+
+        if (vectorVelocity.x > 0)
+            vectorVelocity.x -= vectorVelocity.x / 150;
+       
+        myRigidbody.velocity = vectorVelocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        switch (coll.gameObject.name)
+        {
+            case "chocolate":
+                Destroy(chocolate);
+                health++;
+                break;
+
+        }
+    }
+
 }
