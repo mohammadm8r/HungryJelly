69188a44ae14c67d602e4008a64895370c3f0665
mohammadm8r <mrezaei76@yahoo.com>
2016-08-01 15:16:29 +0430

    First commit

---
+++ Assets/scripts/PlayerHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHandler : MonoBehaviour {
+
+    private Rigidbody2D myRigidbody;
+    [SerializeField]
+    private float speed;
+    private Vector2 vectorVelocity;
+
+	// Use this for initialization
+	void Start () {
+
+        myRigidbody = GetComponent<Rigidbody2D>();
+        vectorVelocity.x = 3;
+        vectorVelocity.y = 6;
+        myRigidbody.velocity = vectorVelocity;
+	}
+	
+	// Update is called once per frame
+    void update()
+    {
+        Debug.LogError(vectorVelocity);
+    }
+   
+    
+}
